3edd4b8367aad3c9844c0afeebb80d03cf7cb1d2
Daisuke <c0a24289bc@edu.teu.ac.jp>
2025-11-18 18:40:44 +0900

    練習10:こうかとんが移動する

--- pygame_image.py
+++ pygame_image.py
@@ -10,26 +10,36 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img2 , True, False)
-    kk_img = pg.image .load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, False)
+    bg_img2 = pg.transform.flip(bg_img, True, False) # 練習8
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False) # 練習3
+    kk_rct=kk_img.get_rect() # 練習10
+    kk_rct.center=300, 200  # 練習10
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2, [-x + 1600, 0])
-        screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kk_img, [300,200])
-        
+        x=tmr%3200
+        screen.blit(bg_img, [-x, 0]) # 練習5
+        screen.blit(bg_img2, [-x+1600, 0]) # 練習8
+        screen.blit(bg_img, [-x+3200, 0]) # 練習9
+         # 練習4
+
+         # 練習10
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0))
+        screen.blit(kk_img, kk_rct) # 練習4 -> 10
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200) # 練習6
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     pg.init()
     main()
     pg.quit()
